a541d4f4c905ce7f659579b587530f7cfe33fc36
riadh-trabelsi <143900385+riadh-trabelsi@users.noreply.github.com>
2024-02-26 19:22:38 +0100

    work in progress

---
+++ adminDashboard/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from './layout/Sidebar';
+
+function App() {
+
+
+  return (
+    <>
+     <Sidebar/>
+    </>
+  )
+}
+
+export default App
---
+++ adminDashboard/src/layout/Sidebar.jsx
@@ -0,0 +1,11 @@
+
+
+function Sidebar() {
+  return (
+    <div className="bg-danger" style={{height:"100vh", width:"300px"}}>
+        Sidebar
+        </div>
+  )
+}
+
+export default Sidebar
